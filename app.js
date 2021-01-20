5da48c1e8a31f2c0b1874a41569ce28bc9578364
Ole Loigu <ole.loigu@avalanchelabs.ee>
2021-01-20 13:55:35 +0200

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+// Object Literals
+
+// Let var const or function keywords
+
+// ES5 & ES6 Classes
+
+// Constructors
+
+// Prototypes & Inheritance
+
+// Object.create()
+
+// Avoiding global variables
+
+// DOM element manipulation

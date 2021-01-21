91e59089e9a770b1d7a0f20c079d323d841c51a3
Ole Loigu <ole.loigu@avalanchelabs.ee>
2021-01-21 19:03:31 +0200

    bird class

--- app/classes/Bird.js
+++ app/classes/Bird.js
@@ -1,1 +1,17 @@
-
+import {getRandomCountry} from '../data/countries';
+import {Animal} from './Animal';
+export class Bird extends Animal {
+	constructor(id, name, animal_name, scientific_name, type, description) {
+		super(id, name, animal_name, scientific_name, type, description);
+		this.migrationFrom = getRandomCountry();
+		this.migrationTo = getRandomCountry();
+		this.birdMigration = () => {
+			alert(
+				`My name is ${this.name} and during the winter I fly from ${this.migrationFrom.name}(${this.migrationFrom.code}) to ${this.migrationTo.name}(${this.migrationTo.code})`
+			);
+		};
+	}
+	birdGreeting = () => {
+		console.log('Im a bird and i fly');
+	};
+}
